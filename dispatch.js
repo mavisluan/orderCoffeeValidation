f66e1bcede18f35bfb6269a46f345a9139cc268f
yi <mavisluan83@gmail.com>
2020-06-09 12:19:52 -0700

    Added orderPizza intent

--- dispatch.js
+++ dispatch.js
@@ -2,6 +2,7 @@
 
 const orderCoffee = require('./orderCoffee');
 const orderTea = require('./orderTea');
+const orderPizza = require('./orderPizza');
 
 module.exports = (intentRequest, callback) => {
     console.log(`dispatch userId=${intentRequest.userId}, intentName=${intentRequest.currentIntent.name}`);
@@ -15,6 +16,11 @@ module.exports = (intentRequest, callback) => {
     if (intentName === 'TeaOrder') {
       console.log(intentName + ' was called');
       return orderTea(intentRequest, callback);
+    }
+
+    if (intentName === 'PizzaOrder') {
+      console.log(intentName + ' was called');
+      return orderPizza(intentRequest, callback);
     }
     throw new Error(`Intent with name ${intentName} not supported`);
   }
---
+++ orderPizza.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const lexResponses = require('./lexResponses');
+
+const types = ['veg', 'cheese', 'pepperoni'];
+const sizes = ['small', 'medium', 'large'];
+const crusts = ['thin', 'thick'];
+
+const buildValidationResult = (isValid, violatedSlot, messageContent) => {
+  if (messageContent == null) {
+      return {
+          isValid,
+          violatedSlot,
+      };
+  }
+  return {
+      isValid,
+      violatedSlot,
+      message: { contentType: 'PlainText', content: messageContent },
+  };
+}
+
+const validatePizzaOrder = (pizzaType, pizzaSize, pizzaCrust) => {
+  if (pizzaType && types.indexOf(pizzaType) === -1) {
+    return buildValidationResult(false, 'pizza', `We do not have ${pizzaType}, would you like a different type of pizza?  Our most popular pizza is pepperoni.`);
+  }
+
+  if (pizzaCrust && crusts.indexOf(pizzaCrust.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'pizzaCrust', `We do not have ${pizzaCrust}, would you like a different crust of pizza? Our most popular crust is thick.`);
+  }
+
+
+  if (pizzaSize && sizes.indexOf(pizzaSize.toLowerCase()) === -1) {
+    return buildValidationResult(false, 'pizzaSize', `We do not have ${pizzaSize}, would you like a different size of pizza? Our most popular size is medium.`);
+  }
+
+  return buildValidationResult(true, null, null);
+}
+
+const buildFulfillmentResult = (fulfillmentState, messageContent) =>{
+  return {
+    fulfillmentState,
+    message: {contentType: 'PlainText', content: messageContent}
+  }
+}
+
+
+module.exports = (intentRequest, callback) => {
+  const pizzaType = intentRequest.currentIntent.slots.pizza;
+  const pizzaCrust = intentRequest.currentIntent.slots.crust;
+  const pizzaSize = intentRequest.currentIntent.slots.pizzaSize;
+  
+  console.log('currentIntentSlots', pizzaType + ' ' + pizzaCrust + ' ' + pizzaSize );
+
+  const source = intentRequest.invocationSource;
+
+  if (source === 'DialogCodeHook') {
+    const slots = intentRequest.currentIntent.slots;
+    const validationResult = validatePizzaOrder(pizzaType, pizzaSize, pizzaCrust);
+
+    if (!validationResult.isValid) { 
+      slots[`${validationResult.violatedSlot}`] = null; // set violatedSlot value to be null
+      // ask for the violatedSlot input again
+      console.log('Input NOT valid, elicit the violated slot again')
+      callback(lexResponses.elicitSlot(intentRequest.sessionAttributes, intentRequest.currentIntent.name, slots, validationResult.violatedSlot, validationResult.message));
+      return;
+    }
+
+    if (pizzaType !== null && pizzaSize !== null) {
+      if (pizzaSize === 'small') intentRequest.sessionAttributes['Price'] = (pizzaType.length * 1.5).toFixed(2);
+      if (pizzaSize === 'medium') intentRequest.sessionAttributes['Price'] = (pizzaType.length * 2).toFixed(2);
+      else intentRequest.sessionAttributes['Price'] = (pizzaType.length * 3).toFixed(2);
+    } 
+
+    callback(lexResponses.delegate(intentRequest.sessionAttributes, intentRequest.currentIntent.slots));
+    return;
+  }
+
+  if (source === 'FulfillmentCodeHook') {
+    console.log('FulfillmentCodeHook');
+
+    const {fulfillmentState, message} = buildFulfillmentResult('Fulfilled', `Your order of a ${pizzaSize} ${pizzaCrust} ${pizzaType} is placed. Your total will be $${intentRequest.sessionAttributes['Price']}. Thank you!`)
+
+    callback(lexResponses.close(intentRequest.sessionAttributes, fulfillmentState, message));
+  }
+}
